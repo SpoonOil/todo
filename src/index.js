571a530c74a5141551eb0c27e9ef8b2e1e5b1d0c
SpoonOil <nieves.jon.8@gmail.com>
2024-04-16 19:44:05 -0400

    add high level controller and task adding skeleton

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import './style.css'
 import DOM from './printDOM.js'
 
 const test = new TaskList
+const taskButton = document.getElementById('add-task-button');
+
+taskButton.addEventListener('click')
 
 test.addTask("bruh", "this is a test task");
 
