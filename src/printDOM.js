bbb5c76d154ab2b5db0f734860f6f857267db457
SpoonOil <nieves.jon.8@gmail.com>
2024-04-22 16:50:37 -0400

    add tasklist li styles to js add helper function for tailwind classes add placeholder html

--- src/printDOM.js
+++ src/printDOM.js
@@ -1,3 +1,8 @@
+function addStyles(node, tailwindStyleString) {
+  const classes = tailwindStyleString.split(' ')
+  node.classList.add(...classes)
+}
+
 class DOMHandler {
   constructor(ulNode, taskList) {
     this.ulNode = ulNode
@@ -5,17 +10,23 @@ class DOMHandler {
   }
   
   renderTaskList() {
+
     while (this.ulNode.firstChild) {
       this.ulNode.firstChild.remove()
     }
 
     for (let task of this.taskList.getTasks()) {
-      const li = document.createElement('li')  
-      li.style.classList = "bg-slate-400"
+      const li = document.createElement('li')
+      addStyles(li, 'flex gap-4 items-center flex-1 p-8 rounded-lg bg-slate-200')
       const title = document.createElement('h3')
       const description =document.createElement('p')
+      addStyles(description, 'flex-1')
+
       const checkbox = document.createElement('input');
+      addStyles(checkbox, 'appearance-none active:border-0 checked:bg-slate-600 checked:border-1 w-5 h-5 border-2 border-slate-500 bg-white rounded-full')
+
       const close = document.createElement('button')
+      addStyles(close, 'bg-red-200 w-8 h-8 rounded-md font-mono')
 
       close.innerText = 'X'
       close.addEventListener('click', () => {
@@ -37,9 +48,9 @@ class DOMHandler {
 
       title.innerText = task.name
       description.innerText = task.description
+      li.appendChild(checkbox)
       li.appendChild(title);
       li.appendChild(description);
-      li.appendChild(checkbox)
       li.appendChild(close)
 
       if (task.completed == "completed") {
