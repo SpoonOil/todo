50bfd1e2a5b60c0edbbe71a6ebff85c3610f3b19
SpoonOil <nieves.jon.8@gmail.com>
2024-04-26 14:10:06 -0400

    refactor task sorting to happen during getTasks() this keep list the same for all other purposes, squashing some bugs idk how to deal with

--- src/tasks.js
+++ src/tasks.js
@@ -2,18 +2,18 @@ class Task {
   constructor(name, description, taskList) {
     this.name = name;
     this.description = description;
-    this.completed = "notCompleted" 
-    this.parentList = taskList.tasks
-    this.parent = taskList
+    this.completed = "notCompleted";
+    this.parentList = taskList.tasks;
+    this.parent = taskList;
     this.id = this.parent.getNextId();
   }
 
   complete() {
-    this.completed = "completed"
+    this.completed = "completed";
   }
 
   uncomplete() {
-    this.completed = "notCompleted"
+    this.completed = "notCompleted";
   }
 }
 
@@ -21,48 +21,47 @@ class TaskList {
   constructor(listName, listDescription) {
     this.name = listName;
     this.description = listDescription;
-    this.tasks = []
-    this.latestIdNum = 0
+    this.tasks = [];
+    this.latestIdNum = 0;
   }
 
   getNextId() {
-    let id = this.latestIdNum
-    this.latestIdNum++
-    return id
+    let id = this.latestIdNum;
+    this.latestIdNum++;
+    return id;
   }
 
   getTasks() {
-    return this.tasks;
-  }
-  addTask(taskName, taskDescription, list) {
-    this.tasks.push(new Task(taskName, taskDescription, this))
-    this.orderTasks();
-  }
-
-  orderTasks() {
-    const completedTasks = []
-    for ( let task of this.tasks) {
+    let completedTasks = [];
+    let uncompletedTasks = []
+    for (let task of this.tasks) {
       if (task.completed == "completed") {
-        completedTasks.push(this.tasks.splice(this.tasks.indexOf(task), 1)[0])
+        completedTasks.push(task);
+      } else {
+        uncompletedTasks.push(task)
       }
     }
-    this.tasks = this.tasks.concat(completedTasks)
+    const sortedTasks = uncompletedTasks.concat(completedTasks)
+    return sortedTasks;
+  }
+  addTask(taskName, taskDescription, list) {
+    this.tasks.push(new Task(taskName, taskDescription, this));
   }
 
   removeTask(taskID) {
-    this.tasks.splice(this.tasks.findIndex((task) => task.name == taskName), 1)
-    this.orderTasks();
+    this.tasks.splice(
+      this.tasks.findIndex((task) => task.id == taskID),
+      1
+    );
   }
-  
+
   completeTask(taskID) {
-    this.tasks.find((task) => task.id == taskID).complete()
-    this.orderTasks();
+    this.tasks.find((task) => task.id == taskID).complete();
   }
 
   uncompleteTask(taskID) {
-    this.tasks.find((task) => task.id == taskID).uncomplete()
-    this.orderTasks();
+    this.tasks.find((task) => task.id == taskID).uncomplete();
   }
 }
 
-export {Task, TaskList}
+export { Task, TaskList };
